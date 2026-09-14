0ea2e09882b21171a7b8b2b17f880b68ffc88df9
Kirill Stankevich <stankevichkirill6@gmail.com>
2026-09-14 15:39:44 +0300

    feat: add TagRepository for search exists tags

--- src/main/java/org/example/noteservice/repository/TagRepository.java
+++ src/main/java/org/example/noteservice/repository/TagRepository.java
@@ -2,8 +2,15 @@ package org.example.noteservice.repository;
 
 import org.example.noteservice.entity.tag.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 
 @Repository
 public interface TagRepository extends JpaRepository<Tag, Long> {
+    @Query("SELECT DISTINCT t FROM tags t WHERE t.name IN :names ORDER BY t.id")
+    List<Tag> findExistsTagByNames (@Param("names") List<String> tagsName);
 }
