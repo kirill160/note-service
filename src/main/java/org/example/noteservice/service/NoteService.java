7ddaaf75ace752de79ef8798151f92defebe1b03
Kirill Stankevich <stankevichkirill6@gmail.com>
2026-09-14 15:43:38 +0300

    fix: resolve duplicate tag constraint violation

--- src/main/java/org/example/noteservice/service/NoteService.java
+++ src/main/java/org/example/noteservice/service/NoteService.java
@@ -1,8 +1,10 @@
 package org.example.noteservice.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.example.noteservice.dto.NoteRequestDTO;
 import org.example.noteservice.dto.NoteResponseDTO;
 import org.example.noteservice.entity.note.Note;
+import org.example.noteservice.entity.tag.Tag;
 import org.example.noteservice.handler.exception.NoteNotFoundException;
 import org.example.noteservice.mapper.NoteMapperImpl;
 import org.example.noteservice.repository.NoteArchiveRepository;
@@ -12,26 +14,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.HashSet;
 import java.util.List;
-import java.util.logging.Logger;
+import java.util.Set;
 
+@Slf4j
 @Service
 public class NoteService {
     private final NoteRepository noteRepository;
     private final NoteArchiveRepository noteArchiveRepository;
     private final NoteSearchRepository noteSearchRepository;
     private final NoteMapperImpl noteMapperImpl;
+    private final TagService tagService;
 
     @Autowired
     public NoteService(NoteRepository noteRepository,
                        NoteArchiveRepository noteArchiveRepository,
                        NoteSearchRepository noteSearchRepository,
-                       NoteMapperImpl noteMapperImpl
+                       NoteMapperImpl noteMapperImpl,
+                       TagService tagService
                        ) {
         this.noteRepository = noteRepository;
         this.noteArchiveRepository = noteArchiveRepository;
         this.noteSearchRepository = noteSearchRepository;
         this.noteMapperImpl = noteMapperImpl;
+        this.tagService = tagService;
 
     }
 
@@ -43,9 +50,14 @@ public class NoteService {
     public NoteResponseDTO findByIdDTO(Long idDto){
         return noteMapperImpl.toNoteResponseDTO(findById(idDto));
     }
-    public NoteResponseDTO save(NoteRequestDTO noteRequestDTO ) {
-        Note note = noteMapperImpl.toNote(noteRequestDTO);
-        return noteMapperImpl.toNoteResponseDTO(noteRepository.save(note));
+
+    @Transactional
+    public NoteResponseDTO save(NoteRequestDTO noteRequestDTO) {
+        List<Tag> tags = tagService.filterRequestTagsAndSaveFilteringTags(noteRequestDTO);
+        Note saveNote = noteMapperImpl.toNote(noteRequestDTO);
+        saveNote.setTags(new HashSet<>(tags));
+        Note noteToDto = noteRepository.save(saveNote);
+        return noteMapperImpl.toNoteResponseDTO(noteToDto);
     }
     @Transactional(timeout = 5, rollbackFor = {NoteNotFoundException.class})
     public void deleteById(Long id) {
--- src/main/java/org/example/noteservice/service/TagService.java
+++ src/main/java/org/example/noteservice/service/TagService.java
@@ -1,4 +1,44 @@
 package org.example.noteservice.service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.example.noteservice.dto.NoteRequestDTO;
+import org.example.noteservice.dto.TagDTO;
+import org.example.noteservice.entity.tag.Tag;
+import org.example.noteservice.repository.TagRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class TagService {
+    private final TagRepository repository;
+
+    private List<Tag> getTagsFromRequestDTO(NoteRequestDTO noteRequestDTO) {
+        List<String> namesTag = noteRequestDTO.getTags()
+                .stream()
+                .map(TagDTO::getName)
+                .toList();
+        return repository.findExistsTagByNames(namesTag);
+    }
+    public List<Tag> filterRequestTagsAndSaveFilteringTags(NoteRequestDTO noteRequestDTO) {
+        Set<Tag> tags = mapToTags(noteRequestDTO);
+        return repository.saveAll(tags);
+    }
+    private Set<Tag> mapToTags(NoteRequestDTO noteRequestDTO) {
+        Set<TagDTO> existsTags = getTagsFromRequestDTO(noteRequestDTO)
+                .stream()
+                .map(tag -> new TagDTO(tag.getName()))
+                .collect(Collectors.toSet());
+        return noteRequestDTO
+                .getTags()
+                .stream()
+                .filter(tagDTO -> !existsTags.contains(tagDTO))
+                .map(tagDTO -> Tag.builder().name(tagDTO.getName()).build())
+                .collect(Collectors.toSet());
+    }
 }
