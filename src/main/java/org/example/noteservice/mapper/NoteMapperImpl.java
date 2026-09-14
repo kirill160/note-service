5686c56b34eb064300fb571af563b7c563ee817c
Kirill Stankevich <stankevichkirill6@gmail.com>
2026-09-14 15:50:40 +0300

    refactor: replacing autowired with RequiredArgsConstructor

--- src/main/java/org/example/noteservice/mapper/NoteMapperImpl.java
+++ src/main/java/org/example/noteservice/mapper/NoteMapperImpl.java
@@ -1,17 +1,25 @@
 package org.example.noteservice.mapper;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.example.noteservice.dto.NoteRequestDTO;
 import org.example.noteservice.dto.NoteResponseDTO;
 import org.example.noteservice.dto.TagDTO;
 import org.example.noteservice.entity.note.Note;
 import org.example.noteservice.entity.tag.Tag;
+import org.example.noteservice.handler.exception.TagAlreadyExistsException;
+import org.example.noteservice.repository.TagRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 @Component
+@RequiredArgsConstructor
 public class NoteMapperImpl implements NoteMapper {
     @Override
     public NoteResponseDTO toNoteResponseDTO(Note note) {
@@ -24,19 +32,10 @@ public class NoteMapperImpl implements NoteMapper {
 
     @Override
     public Note toNote(NoteRequestDTO noteRequestDTO) {
-        Set<Tag> tagSet = noteRequestDTO.getTags()
-                .stream()
-                .map(noteDTO -> Tag
-                        .builder()
-                        .name(noteDTO.getName())
-                        .build()
-                )
-                .collect(Collectors.toSet());
         return Note.builder()
                 .title(noteRequestDTO.getTitle())
                 .content(noteRequestDTO.getTextOfNote())
                 .archive(noteRequestDTO.isArchive())
-                .tags(tagSet)
                 .build();
 
     }
