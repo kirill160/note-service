3ebccbbba31a8a449b19fa8928330a4beb50b62b
Kirill Stankevich <stankevichkirill6@gmail.com>
2026-09-15 12:01:28 +0300

    refactor: moved caching annotations out of the controller add transactional annotation

--- src/main/java/org/example/noteservice/service/NoteService.java
+++ src/main/java/org/example/noteservice/service/NoteService.java
@@ -7,36 +7,31 @@ import org.example.noteservice.entity.note.Note;
 import org.example.noteservice.entity.tag.Tag;
 import org.example.noteservice.handler.exception.NoteNotFoundException;
 import org.example.noteservice.mapper.NoteMapperImpl;
-import org.example.noteservice.repository.NoteArchiveRepository;
 import org.example.noteservice.repository.NoteRepository;
-import org.example.noteservice.repository.NoteSearchRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+
 import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 @Slf4j
 @Service
 public class NoteService {
     private final NoteRepository noteRepository;
-    private final NoteArchiveRepository noteArchiveRepository;
-    private final NoteSearchRepository noteSearchRepository;
     private final NoteMapperImpl noteMapperImpl;
     private final TagService tagService;
 
     @Autowired
     public NoteService(NoteRepository noteRepository,
-                       NoteArchiveRepository noteArchiveRepository,
-                       NoteSearchRepository noteSearchRepository,
                        NoteMapperImpl noteMapperImpl,
                        TagService tagService
                        ) {
         this.noteRepository = noteRepository;
-        this.noteArchiveRepository = noteArchiveRepository;
-        this.noteSearchRepository = noteSearchRepository;
         this.noteMapperImpl = noteMapperImpl;
         this.tagService = tagService;
 
@@ -47,11 +42,12 @@ public class NoteService {
                 .findById(id)
                 .orElseThrow(() -> new NoteNotFoundException(String.format("Note with id %d not found", id)));
     }
+    @Cacheable(cacheNames = "notes", key ="#idDto")
     public NoteResponseDTO findByIdDTO(Long idDto){
         return noteMapperImpl.toNoteResponseDTO(findById(idDto));
     }
-
     @Transactional
+    @CachePut(cacheNames = "notes", key = "#result.id")
     public NoteResponseDTO save(NoteRequestDTO noteRequestDTO) {
         List<Tag> tags = tagService.filterRequestTagsAndSaveFilteringTags(noteRequestDTO);
         Note saveNote = noteMapperImpl.toNote(noteRequestDTO);
@@ -59,28 +55,30 @@ public class NoteService {
         Note noteToDto = noteRepository.save(saveNote);
         return noteMapperImpl.toNoteResponseDTO(noteToDto);
     }
+
+    @CacheEvict(cacheNames = "notes", key = "#id")
     @Transactional(timeout = 5, rollbackFor = {NoteNotFoundException.class})
     public void deleteById(Long id) {
-        Note note = findById(id);
-        noteRepository.delete(note);
-
+            noteRepository.deleteById(id);
     }
+
+    @CachePut(cacheNames = "notes", key = "#id" )
     @Transactional(timeout = 5, rollbackFor = {NoteNotFoundException.class})
-    public NoteResponseDTO saveAndSetArchiveNote(Long id, boolean save) {
+    public NoteResponseDTO updateArchiveStatus(Long id, boolean save) {
         Note note = findById(id);
         note.setArchive(save);
-        return noteMapperImpl.toNoteResponseDTO(noteArchiveRepository.save(note));
+        return noteMapperImpl.toNoteResponseDTO(noteRepository.save(note));
     }
-
+    @Transactional(readOnly = true)
     public List<NoteResponseDTO> findNotesByArchive(boolean archive) {
-        return noteMapperImpl.toNotesResponseDTO(noteArchiveRepository.getNotesByArchive(archive));
+        return noteMapperImpl.toNotesResponseDTO(noteRepository.getNotesByArchive(archive));
     }
-
+    @Transactional(readOnly = true)
     public List<NoteResponseDTO> searchByTag(List<String> tags) {
-        return noteMapperImpl.toNotesResponseDTO(noteSearchRepository.findNotesByTags(tags));
+        return noteMapperImpl.toNotesResponseDTO(noteRepository.findNotesByTags(tags));
     }
-
+    @Transactional(readOnly = true)
     public List<NoteResponseDTO> search(String searchString) {
-        return noteMapperImpl.toNotesResponseDTO(noteSearchRepository.findNotesByQuery(searchString));
+        return noteMapperImpl.toNotesResponseDTO(noteRepository.findNotesByQuery(searchString));
     }
 }
